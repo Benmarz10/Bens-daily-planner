39bb7a1c77ceecb560f0deed0088c63806cbcc43
Benmarz10 <benjaminmarz10@gmail.com>
2022-11-28 13:27:11 -0600

    Updated readme, made js file easier to look at

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is a simple application that will allow the users to plan out their work days. The allowed time blocks are from 9am to 5pm, each timeblock is easily readable because of the color coding. Past time blocks will be displayed gray and present in red while future time blocks are displayed green. The user can also input descriptions in each timeblock and save the description.
 
 ## Links
-
+https://github.com/ryan-harris/day-planner/blob/master/script.js - I used this as a reference to figure out how to color code each timeblock accordingly.
 
 ## Screenshot
 
--- script.js
+++ script.js
@@ -3,15 +3,16 @@ var today = dayjs();
 $(init);
 
 function init() {
-//Jquery for current date
+//dayjs for current date
 $('#currentDay').text(today.format('MMM D, YYYY'));
 
+//this sets the color coding for each block and puts it on an interval to change every minute
 defineTimeBlocks();
 setInterval(defineTimeBlocks, 60000);
 
 $(".time-block").each(function() {
     var blockId = $(this).attr("id");
-    // load saved data from local storage
+    //Gets data from local storage
     $("#" + blockId + " textarea").text(localStorage.getItem(dayjs().format("DDDYYYY") + blockId));
   });
 
@@ -20,25 +21,26 @@ $(".time-block").each(function() {
 
     function defineTimeBlocks(){
         $(".time-block").each(function(){
+           
             var HourBlock = parseInt($(this).attr("id").replace("hour-", ""));
             var currentHour = parseInt(dayjs().format("H"));
-            $(this).removeClass("past present future");
+            
+            $(this).removeClass("future past present");
+            
             if (HourBlock < currentHour) {
                 $(this).addClass("past");
-            } else if (HourBlock > currentHour) {
+            } 
+            else if (HourBlock > currentHour) {
                 $(this).addClass("future");
-            } else {
+            } 
+            else {
                 $(this).addClass("present")
             }
 
         })
     }
 
+    //set data in the local storage
     function clickSave() {
         $('#' + blockId + " textarea").text(localStorage.setItem(dayjs().format("DDDYYYY") + blockId));
     }
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
